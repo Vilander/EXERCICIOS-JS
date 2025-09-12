cf18d194102836e524fc44633e5a03f17efa9e5d
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-09-11 22:15:52 -0300

    feat: Resolução de exercicio 001

---
+++ EXERCICIO001/app.js
@@ -0,0 +1,10 @@
+let nome="", idade="0";
+
+nome= prompt("Informe seu nome: ")
+idade = prompt("Infrome sua idade: ")
+
+alert("Olá "+nome+" seja bem vindo! \nVocê tem "+idade+" anos");
+
+console.log("Olá "+nome+" seja bem vindo! \nVocê tem "+idade+" anos");
+
+document.write("Olá "+nome+" seja bem vindo! \nVocê tem "+idade+" anos");
