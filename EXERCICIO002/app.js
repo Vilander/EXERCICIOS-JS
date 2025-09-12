75daa38c5c457a8b50b9994c2b1d7d8a65b36a74
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-09-12 20:18:57 -0300

    feat: exercicio 002 executando contas

---
+++ EXERCICIO002/app.js
@@ -0,0 +1,24 @@
+let n1=10, n2=2
+
+console.log(n1+n2) //+ operador de adição
+document.getElementById("resultado_soma").innerText=n1+n2
+
+console.log(n1-n2) //- operador de subtração
+document.getElementById("resultado_sub").innerText=n1+n2
+
+console.log(n1*n2) //* operador de multiplicação
+document.getElementById("resultado_multiplicacao").innerText=n1-n2
+
+console.log(n1/n2) // / operador de divisão
+document.getElementById("resultado_div").innerText=n1/n2
+
+console.log(n1%n2) // % operador de módulo, pega o resto da divisão
+document.getElementById("resultado_modulo").innerText=n1%n2
+
+console.log(n1**n2) // ** operador de exponenciação, pega um número e eleva a outro.  Exemplo: 10²
+document.getElementById("resultado_exponenciacao").innerText=n1**n2
+
+
+
+
+
