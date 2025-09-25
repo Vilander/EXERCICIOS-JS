cf66113c57e110f085614ca2f44d6b79bd969de6
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-09-25 20:39:17 -0300

    feat: exercicio 008

---
+++ EXERCICIO008/app.js
@@ -0,0 +1,20 @@
+function fnCalcular(){
+    let valor1=0,valor2=0, operacao="",resultado=""
+    valor1=parseFloat(document.getElementById("valor1").value)
+    valor2=parseFloat(document.getElementById("valor2").value)
+
+    if(document.getElementById("adicao").checked == true){
+        resultado=valor1+valor2
+    }
+    if (document.getElementById("subtracao").checked == true){
+        resultado=valor1-valor2
+    }
+    if (document.getElementById("multiplicacao").checked == true){
+        resultado=valor1*valor2
+    }
+    if (document.getElementById("divisao").checked == true){
+        resultado=valor1/valor2
+    }
+
+    document.getElementById("res").innerHTML=resultado
+}
