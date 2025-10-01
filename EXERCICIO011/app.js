5bc8d044b016ad03dbac9972aaa6b47c772b51ec
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-09-30 21:02:13 -0300

    feat: exercicio 11

---
+++ EXERCICIO011/app.js
@@ -0,0 +1,25 @@
+let listaDeNumeros = []
+
+function fnLimpar(){
+    fnSalvarNumero()
+    document.getElementById("formulario").reset()
+}
+
+function fnSalvarNumero(){
+    let numero=parseFloat(document.getElementById("numero").value)
+    listaDeNumeros.push(numero)
+    // console.log("teste: ",listaDeNumeros)
+}
+
+function fnCalcularMedia(){
+    let media = 0, qtdNumeros = 0, somaDosNumeros=0, i=0
+    qtdNumeros = listaDeNumeros.length
+    
+    while(i<qtdNumeros){
+        somaDosNumeros+=listaDeNumeros[i]
+        i++
+    }
+    media=somaDosNumeros/qtdNumeros
+    document.getElementById("media").innerHTML=media
+    listaDeNumeros=[]
+}
