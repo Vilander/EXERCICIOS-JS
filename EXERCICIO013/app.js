e129cc502e6e16e2f2370ce92c282deac5b5c02c
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-10-06 22:23:46 -0300

    feat: implementado função para executar listagem

---
+++ EXERCICIO013/app.js
@@ -0,0 +1,29 @@
+let numeroDeVagas=[]
+let veiculos = []
+
+function fnSalvarVeiculo(){
+    numeroDeVagas.push(document.getElementById("numero-vaga").value)
+    veiculos.push(document.getElementById("veiculo").value)
+    
+}
+
+
+function fnListarVeiculo(){
+    // console.dir(numeroDeVagas)
+    // console.dir(veiculos)
+    /*
+    let i = 0
+    while(i < veiculos.length){
+        console.log(veiculos[i], numeroDeVagas[i])
+        i++
+    }
+    */
+    //for(declaração contador; condição; incremento){}
+    document.getElementById("tabela_veiculos").innerHTML=''
+    for(let i = 0; i < veiculos.length; i++){
+        document.getElementById("tabela_veiculos").innerHTML +=`<tr><td>${numeroDeVagas[i]}</td><td>${veiculos[i]}</td></tr>`
+        
+    }
+}
+
+function fnLimpar(){}
